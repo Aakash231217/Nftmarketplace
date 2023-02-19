011f4995666ee72b3c8e1d53212ad916409bad35
AKninjas <rktaakash@gmail.com>
2023-02-19 23:15:00 +0530

    first commit

---
+++ context/constants.js
@@ -0,0 +1,7 @@
+import market from './NFTMarketplace.json';
+
+export const MarketAddress = process.env.NEXT_PUBLIC_MARKET_ADDRESS;
+export const MarketAddressABI = market.abi;
+
+// eslint-disable-next-line eol-last
+/* 0x5FbDB2315678afecb367f032d93F642f64180aa3 */
